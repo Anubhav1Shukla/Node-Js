fb2d4119848b35774850cae651e7981ce482dcfb
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-18 20:08:20 +0530

    Node js Tutorial by piyush Garg

---
+++ file.js
@@ -0,0 +1,32 @@
+const fs=require('fs');
+
+//sync
+// fs.writeFile('./test.txt','Hey Anubhav');
+
+//Async
+// fs.writeFile("./test.txt","Hello World Async",(err)=>{});
+
+//Sync:-Ye return karta hai 
+// const result=fs.readFileSync("./contact.txt","utf-8");
+// console.log(result);
+
+
+
+//Async:-ye kuch return nahi karta hai callback accept karta hai hamesa
+// const result=fs.readFile("./contact.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("Error",err)
+//     }else{
+//         console.log(result);
+//     }
+// });
+
+
+// fs.appendFileSync("./test.txt","You are good boy");
+// fs.cpSync("./test.txt","./copy.txt");  //copy one file to another file
+
+// fs.unlinkSync("./copy.txt"); //isase ham file ko delete bhi kar sakte hai
+
+
+// console.log(fs.statSync("./test.txt")) file statics 
+
