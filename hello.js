494f83485ed2ab3c6ce3f134a09e1c127bfc5f22
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-18 09:40:30 +0530

    Node js Tutorial by piyush Garg

--- hello.js
+++ hello.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 console.log("Hey there! I am JS");
-=======
-// console.log("Hey there! I am JS");
->>>>>>> 0a59611 (Node js Tutorial by piyush Garg)
 
 
 //Jitni bhi design related word hai vo sab compiler pe  run nahi kar sakte only chrome ke cosole me jaake karna padega aisa nahi hai ki chrome me v8 engine direct use kar liya gya 
@@ -17,12 +13,4 @@ console.log("Hey there! I am JS");
 // package.json file ek tarah ki configuration file hai is particular project ka 
 // jo bhi dependencies ham install karnge ya jab bhi ham isko publish karenge aur run karenge to sarra package.json file me hoga 
 
-<<<<<<< HEAD
 //Window objects available nahi hai
-=======
-//Window objects available nahi hai
-
-
-const math=require("./math")
-console.log(math(5,6))
->>>>>>> 0a59611 (Node js Tutorial by piyush Garg)
