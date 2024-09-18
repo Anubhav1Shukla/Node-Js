8f56d8733f8e0451780e7168e9e8a8601c6c6828
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-18 09:06:40 +0530

    Node js Tutorial by piyush Garg

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Hey there! I am JS");
+
+
+//Jitni bhi design related word hai vo sab compiler pe  run nahi kar sakte only chrome ke cosole me jaake karna padega aisa nahi hai ki chrome me v8 engine direct use kar liya gya 
+
+
+
+// commonds
+// node file.js name
+// node -v
+// node init 
+
+// package.json file ek tarah ki configuration file hai is particular project ka 
+// jo bhi dependencies ham install karnge ya jab bhi ham isko publish karenge aur run karenge to sarra package.json file me hoga 
+
+//Window objects available nahi hai
