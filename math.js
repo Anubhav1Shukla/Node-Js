ae3cf78632ba2923654af776bdd26c106d9ba7dc
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-18 09:31:57 +0530

    Node js Tutorial by piyush Garg

---
+++ math.js
@@ -0,0 +1,7 @@
+//jab hame apne pure codebase ko chote chote chote parts me divide kar dete hai to use bolte hai mudular programiiing
+
+function add(a,b){
+    return a+b;
+}
+
+module.exports=add;
