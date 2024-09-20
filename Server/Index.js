013f2fd8f83b0f422dfdab098830047b5e4a069a
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-20 20:28:27 +0530

    Node Js by piyush Garg

--- Server/Index.js
+++ Server/Index.js
@@ -1,16 +1,27 @@
 const http=require("http");
 const fs=require('fs');
-
+const url=require("url");
+const { transferableAbortController } = require("util");
 const myServer=http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
     const log=`${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl=url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/":
                 res.end("HomePage");
                 break;
             case "/about":
-                res.end("Hey! I am Anubhav here.");
+                const username=myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                // res.end("Hey! I am Anubhav here.");
                 break;
+
+            case "/search":
+                const search=myUrl.query.search_query;
+                res.end("Here is your Searching content "+search);
+               
             default:
                 res.end("404 Not Found!")
         }
@@ -19,3 +30,14 @@ const myServer=http.createServer((req,res)=>{
 });
 
 myServer.listen(8000,()=> console.log("Server Started......"));
+
+
+
+
+// URl--->Uniform Resource Locator
+
+// Protocal
+// hypertext
+// transfer
+// Protocal
+// secure
