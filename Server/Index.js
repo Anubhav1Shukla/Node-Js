bcd3aa27255948bdf7ab98cea5a4d0888c9b6c2d
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-22 01:14:48 +0530

    Node Js by Piyush garg

--- Server/Index.js
+++ Server/Index.js
@@ -1,40 +1,62 @@
 const http=require("http");
-const fs=require('fs');
-const url=require("url");
-const { transferableAbortController } = require("util");
-const myServer=http.createServer((req,res)=>{
-    if(req.url==="/favicon.ico") return res.end();
-    const log=`${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-    const myUrl=url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile('log.txt',log,(err,data)=>{
-        switch(myUrl.pathname){
-            case "/":
-                if(req.method==="GET") return res.end("You are in home page");
-                break;
-            case "/about":
-                const username=myUrl.query.myname;
-                res.end(`Hi, ${username}`);
-                // res.end("Hey! I am Anubhav here.");
-                break;
+// const fs=require('fs');
+// const url=require("url");
+const express=require("express");
 
-            case "/search":
-                const search=myUrl.query.search_query;
-                res.end("Here is your Searching content "+search);
-            case "/signup":
-                if(req.method==="GET")  res.end("This is a singup Form");
-                else if(req.method=="POST"){
-                    //DB query
-                    res.send("Success");
-                } 
-            default:
-                res.end("404 Not Found!")
-        }
-    })
-   
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    return res.send("Hello From Home Page) :");
 });
 
-myServer.listen(8000,()=> console.log("Server Started......"));
+app.get("/about",(req,res)=>{
+    return res.send(`Hello ${req.query.name}`);
+});
+
+
+app.listen(8000,()=>{
+    console.log("Server is running in port number 8000..........");
+})
+
+
+
+// function myHandler(req,res){
+// const myServer=http.createServer((req,res)=>{
+//     if(req.url==="/favicon.ico") return res.end();
+//     const log=`${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+//     const myUrl=url.parse(req.url,true);
+//     console.log(myUrl);
+//     fs.appendFile('log.txt',log,(err,data)=>{
+//         switch(myUrl.pathname){
+//             case "/":
+//                 if(req.method==="GET") return res.end("You are in home page");
+//                 break;
+//             case "/about":
+//                 const username=myUrl.query.myname;
+//                 res.end(`Hi, ${username}`);
+//                 // res.end("Hey! I am Anubhav here.");
+//                 break;
+
+//             case "/search":
+//                 const search=myUrl.query.search_query;
+//                 res.end("Here is your Searching content "+search);
+//             case "/signup":
+//                 if(req.method==="GET")  res.end("This is a singup Form");
+//                 else if(req.method=="POST"){
+//                     //DB query
+//                     res.send("Success");
+//                 } 
+//             default:
+//                 res.end("404 Not Found!")
+//         }
+//     })
+   
+// });
+// }
+
+// const myServer=http.createServer(app);
+// myServer.listen(8000,()=> console.log("Server Started......"));
 
 
 
